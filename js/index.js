1a1273fba09f2b321db407e4127f235416fe9100
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-06-20 07:07:08 -0600

    Various and sundry updates
    * Updated styles to be more responsive
    * Added an option to personalize the greeting if you aren't me
    * Updated time field
    * Added function to rotate images

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+let visitor
+let todoList
+
 //Set Background with random image from unsplash
 const setBackground = () => {
     fetch(`https://source.unsplash.com/1600x900/?wunderlust`)
@@ -5,25 +8,62 @@ const setBackground = () => {
             document.body.style.background = `url("${response.url}")`;
         })
 }
-setBackground();
 
-//Set greeting
-const today = new Date()
-const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const timeOptions = { hour: 'numeric', minute: 'numeric' }
-const curHr = today.getHours()
-
-if (curHr < 12) {
-    document.getElementById("greeting").innerHTML = "Good morning, Eryn!"
-} else if (curHr < 18) {
-    document.getElementById("greeting").innerHTML = "Good afternoon, Eryn!"
-} else {
-    document.getElementById("greeting").innerHTML = "Good evening, Eryn!"
+//Set name
+const setName = () => {
+    if (localStorage.getItem('name') !== null) {
+        visitor = localStorage.getItem('name');
+        $('#temp').remove();
+    }
 }
 
-document.getElementById("date").innerText =`${today.toLocaleDateString(undefined, dateOptions)}`
-document.getElementById("time").innerText = `${today.toLocaleTimeString('en-US', timeOptions)}`;
+//Set greeting
+const setGreeting = () => {
+    const today = new Date()
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric' }
+    const curHr = today.getHours()
 
+    setName();
+
+    if (visitor !== undefined) {
+        if (curHr < 12) {
+            document.getElementById("greeting").innerHTML = "Good morning, " + visitor + "!"
+        } else if (curHr < 18) {
+            document.getElementById("greeting").innerHTML = "Good afternoon, " + visitor + "!"
+        } else {
+            document.getElementById("greeting").innerHTML = "Good evening, " + visitor + "!"
+        }
+    }
+
+    document.getElementById("date").innerText =`${today.toLocaleDateString(undefined, dateOptions)}`
+    document.getElementById("time").innerText = `${today.toLocaleTimeString('en-US', timeOptions)}`;
+}
+
+//Set name and greeting on load
+const setOnLoad = () => {
+    setName();
+    setGreeting();
+}
+
+window.onload = setOnLoad();
+
+setInterval(setGreeting, 60000);
+
+setInterval(setBackground, 300000);
+
+
+//Add new to do from input box with enter press
+$("input[id='temp']").keypress(function(event){
+    //Note: 'enter' keypress = 13
+    if(event.which === 13){
+        visitor = $(this).val();
+        localStorage.setItem('name', visitor)
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+    }
+});
 
 //Mark to do as completed
 //Set .on listener to existing element ("#container") then on
@@ -41,8 +81,8 @@ $("#container").on("click", ".delete", function(event){
 });
 
 //Add new to do from input box with enter press
-$("input[type='text']").keypress(function(event){
-    //Note: 'enter' keypress which = 13
+$("input[id='todo']").keypress(function(event){
+    //Note: 'enter' keypress = 13
     if(event.which === 13){
         let newTodoText = $(this).val();
         $("#list").prepend("<div class='new-to-do'><div class='delete'><i class='far fa-trash-alt'></i></div><div class='text'> " + newTodoText + "</div></div>");
@@ -51,11 +91,11 @@ $("input[type='text']").keypress(function(event){
 });
 
 //Change placeholder text on focus
-$("input[type='text']").focus(function() {
-    $(this).attr("placeholder", "Type your to-do item here");
+$("input[id='todo']").focus(function() {
+    $(this).attr("placeholder", "  Type your to-do item here");
 });
 
 //Change placeholder text on off focus
-$("input[type='text']").focusout(function() {
-    $(this).attr("placeholder", "What do you have planned today?");
+$("input[id='todo']").focusout(function() {
+    $(this).attr("placeholder", "  What do you have planned today?");
 });
